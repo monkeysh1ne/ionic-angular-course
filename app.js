9ce50aecf0012e02318e2481e5a7baa979dafb11
monkeysh1ne <mcdoob@gmail.com>
2020-04-07 09:06:49 +1200

    add expense list and styling updates

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const reasonInput = document.querySelector('#input-reason');
 const amountInput = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
 
+let totalExpenses = 0;
+
+const clear = () => {
+    reasonInput.value = '';
+    amountInput.value = '';
+};
 
 confirmBtn.addEventListener('click', () => {
     const enteredReason = reasonInput.value;
@@ -11,9 +19,16 @@ confirmBtn.addEventListener('click', () => {
     if (enteredReason.trim().length <= 0 || enteredAmount <= 0 || enteredAmount.trim().length <=0) {
         return;
     }
-    console.log(enteredReason, enteredAmount);
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = enteredReason + ': $' + enteredAmount;
+
+    expensesList.appendChild(newItem);
+
+    totalExpenses += +enteredAmount;
+    totalExpensesOutput.textContent = totalExpenses;
+
+
+    clear();
 });
 
-cancelBtn.addEventListener('click', () => {
-    console.log('cancel clicked')
-});
+cancelBtn.addEventListener('click', clear);
